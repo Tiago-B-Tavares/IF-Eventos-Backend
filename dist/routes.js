56ab4914c650c009cc80615ffe52fca16f893cd7
Tiago-B-Tavares <tiagobritot2016@gmail.com>
2025-01-09 15:42:22 -0300

    teste deploy api 4

--- dist/routes.js
+++ dist/routes.js
@@ -1,35 +1,7 @@
 "use strict";
-var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    var desc = Object.getOwnPropertyDescriptor(m, k);
-    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
-      desc = { enumerable: true, get: function() { return m[k]; } };
-    }
-    Object.defineProperty(o, k2, desc);
-}) : (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    o[k2] = m[k];
-}));
-var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
-    Object.defineProperty(o, "default", { enumerable: true, value: v });
-}) : function(o, v) {
-    o["default"] = v;
-});
-var __importStar = (this && this.__importStar) || function (mod) {
-    if (mod && mod.__esModule) return mod;
-    var result = {};
-    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
-    __setModuleDefault(result, mod);
-    return result;
-};
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.router = void 0;
-const express_1 = __importStar(require("express"));
-const multer_1 = __importDefault(require("multer"));
-const path_1 = __importDefault(require("path"));
+const express_1 = require("express");
 const CreateWebUserController_1 = require("./controllers/webUser/CreateWebUserController");
 const AuthWebUserController_1 = require("./controllers/webUser/AuthWebUserController");
 const DetailWebUserController_1 = require("./controllers/webUser/DetailWebUserController");
@@ -60,11 +32,9 @@ const CreateInscricoesController_1 = require("./controllers/inscricoes/CreateIns
 const RemoveInscricaoController_1 = require("./controllers/inscricoes/RemoveInscricaoController");
 const ShowInscritosByAtividadeController_1 = require("./controllers/inscricoes/ShowInscritosByAtividadeController");
 const ChangeUserPermissionsController_1 = require("./controllers/admin/ChangeUserPermissionsController");
-const multer_2 = __importDefault(require("./config/multer"));
 const CheckOutUserController_1 = require("./controllers/appUser/CheckOutUserController");
 const router = (0, express_1.Router)();
 exports.router = router;
-const upload = (0, multer_1.default)(multer_2.default.upload('./tmp'));
 router.put('/permissions', new ChangeUserPermissionsController_1.ChangeUserPermissionsController().handle);
 // Web User
 router.post('/user', new CreateWebUserController_1.CreateWebUserController().handle);
@@ -103,4 +73,4 @@ router.post('/inscrever', new CreateInscricoesController_1.CreateInscricoesContr
 router.delete('/inscrever', new RemoveInscricaoController_1.RemoveInscricaoController().handle);
 router.get('/inscricoes', new ShowInscritosByAtividadeController_1.ShowInscritosByAtividadeController().handle);
 router.get('/myInscriptions', new ShowInscritosByAtividadeController_1.ShowInscritosByAtividadeController().handle);
-router.use('/files', express_1.default.static(path_1.default.join(__dirname, '..', 'public/uploads')));
+router.get('/favicon.ico', (req, res) => res.status(204)); // Retorna "No Content" para requisições de favicon
